c9e8a9b2bfc9b57dcb2514805473c7f342ce1451
alimasyhuriasghor <alimasyhuriasghor6@gmail.com>
2024-08-01 20:51:53 +0700

    ✨[feature]: Added additional parameter (keyword argument scale), and increase it up to 10x10.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import segno
 # Create a main function to encapsulates every statements
 def main() -> None:
     qrcode = segno.make_qr("Hello Qr!")
-    qrcode.save("qr_pertamaku.png")
+    qrcode.save("qr_pertamaku.png",scale=10)
 
 # Create a top-level code environment
 if __name__ == '__main__':
